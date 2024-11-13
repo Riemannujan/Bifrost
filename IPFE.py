eb1ec13e5ba51e9380795935b411cd9293c54465
Riemannujan <42861617+Riemannujan@users.noreply.github.com>
2024-11-13 17:14:52 +0200

    Update and rename ipfe.py to IPFE.py

--- IPFE.py
+++ IPFE.py
@@ -1,23 +1,7 @@
 from utils import *
 
-"""
-# q
-coef_modulus = 2**31 - 1
-# Size of the polynomial, n in the paper
-t = 2 ** 4
-poly_modulus = init_poly_modulus(t)
-# Size of the vector, l in the paper
-size = 5
-# Bounds on x and y
-Bx = 10
-By = 10
-K = size * Bx * By + 1
-delta = coef_modulus // K
-"""
 
-#====================================================================================================================
-
-def setup(
+def IPFE_setup(
     coef_modulus: int,
     poly_modulus: np.ndarray,
     size: int,
@@ -32,7 +16,7 @@ def setup(
     b = [a * s[i] + e[i] for i in range(size)]
     return s, a, b
 
-def encrypt(
+def IPFE_encrypt(
     msg: list,
     pk0: QuotientRingPoly,
     pk1: list,
@@ -53,7 +37,7 @@ def encrypt(
     c1 = [pk1[i] * u + e1[i] + cst_poly(delta * msg[i], unit, coef_modulus, poly_modulus) for i in range(size)]
     return c0, c1
 
-def keyder(
+def IPFE_keyder(
     sk: list,
     y: list,
     coef_modulus: int,
@@ -65,7 +49,7 @@ def keyder(
         dk += cst_poly(y[i], sk[i], coef_modulus, poly_modulus)
     return dk
 
-def decrypt(
+def IPFE_decrypt(
     dk: QuotientRingPoly,
     y: list,
     c0: QuotientRingPoly,
@@ -80,32 +64,4 @@ def decrypt(
         msg += cst_poly(y[i], c1[i], coef_modulus, poly_modulus)
     msg = round((msg - c0 * dk).coef[0] / delta)
     return msg
-
-
-#=======================================================================================================================
-
-"""
-# Setup the keys
-sk, pk0, pk1 = setup(coef_modulus, poly_modulus, size)
-# Generate random small message in R_t
-msg = random_uniform_vector(size, Bx)
-
-# Generate random weight vector
-y = random_uniform_vector(size, By)
-
-# Encrypt the message into two ciphertexts
-c0, c1 = encrypt(msg, pk0, pk1, coef_modulus, poly_modulus, size, delta)
-
-# Decryption key
-dk = keyder(sk, y, coef_modulus, poly_modulus, size)
-
-# Decrypt the message and return the noise
-msg_decr = decrypt(dk, y, c0, c1, coef_modulus, poly_modulus, delta, size)
-
-res = sum(msg[i] * y[i] for i in range(size))
-
-print(msg_decr)
-print(res)
-"""
-
 
